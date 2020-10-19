5855f3b3ef54cefd880176343e46a393aaf66ce7
Duongtsnc1 <duongbui.hlnm@gmail.com>
2020-10-19 23:45:16 +0700

    dang nhap

--- QuanLyKhoHang/BUS/Dangnhap_BUS.cs
+++ QuanLyKhoHang/BUS/Dangnhap_BUS.cs
@@ -11,6 +11,13 @@ namespace QuanlyKhohang.BUS
 {
     class Dangnhap_BUS
     {
-       
+        public int CheckUser(string user, string pass)
+        {
+            DataTable dt = DataAccess.Query("Dangnhap_check",
+                new SqlParameter("@tk", user),
+                new SqlParameter("@mk", pass));
+            int res = int.Parse(dt.Rows[0][0].ToString());
+            return res;
+        }
     }
 }
