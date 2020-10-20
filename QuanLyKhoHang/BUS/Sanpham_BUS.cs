efb7225d92e5887867d976e584b779fd7d9fc36f
lelinh <lelinhmtatn@gmail.com>
2020-10-20 08:20:53 +0700

    ThemSuaXoa_SP

--- QuanLyKhoHang/BUS/Sanpham_BUS.cs
+++ QuanLyKhoHang/BUS/Sanpham_BUS.cs
@@ -30,5 +30,34 @@ namespace QuanlyKhohang.BUS
             bangDuLieu.Columns[4].HeaderText = "Số lượng";
             bangDuLieu.Columns[5].Visible = false;
         }
+        public void timKiem(string tensp, string tenncc)
+        {
+            dv.RowFilter = "[TenSP] like '%" + tensp + "%' and [TenNCC] like '%" + tenncc + "%'";
+            bangDuLieu.DataSource = dv;
+        }
+        public void Add(string tensp, int nccid, float gia, int soluong)
+        {
+            DataAccess.NonQuery("Sanpham_insert",
+                new SqlParameter("@ten", tensp),
+                new SqlParameter("@nccid", nccid),
+                new SqlParameter("@gia", gia),
+                new SqlParameter("@soluong", soluong));
+        }
+        public int Delete(int spid)
+        {
+            DataTable dt = DataAccess.Query("Sanpham_delete",
+                new SqlParameter("@spid", spid));
+            int ret = int.Parse(dt.Rows[0][0].ToString());
+            return ret;
+        }
+        public void Update(int spid, string tensp, int nccid, float gia)
+        {
+            DataAccess.NonQuery("Sanpham_update",
+                new SqlParameter("@id", spid),
+                new SqlParameter("@ten", tensp),
+                new SqlParameter("@nccid", nccid),
+                new SqlParameter("@gia", gia));
+        }
+
     }
 }
