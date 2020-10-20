f0b3e74ab3cefc2b77c59c56b047ff30c852db23
lelinh <lelinhmtatn@gmail.com>
2020-10-20 08:19:25 +0700

    SanPham_bus

--- QuanLyKhoHang/BUS/Sanpham_BUS.cs
+++ QuanLyKhoHang/BUS/Sanpham_BUS.cs
@@ -13,5 +13,22 @@ namespace QuanlyKhohang.BUS
     class Sanpham_BUS
     {
 
+        #region Control
+        public DataGridView bangDuLieu { get; set; }
+        public DataView dv { get; set; }
+        #endregion
+        public void ViewAll()
+        {
+            string sql = string.Format("Sanpham_view");
+            DataTable dt = DataAccess.Query(sql);
+            bangDuLieu.DataSource = dt;
+            dv = new DataView(dt);
+            bangDuLieu.Columns[0].HeaderText = "ID";
+            bangDuLieu.Columns[1].HeaderText = "Tên sản phẩm";
+            bangDuLieu.Columns[2].HeaderText = "Nhà cung cấp";
+            bangDuLieu.Columns[3].HeaderText = "Giá";
+            bangDuLieu.Columns[4].HeaderText = "Số lượng";
+            bangDuLieu.Columns[5].Visible = false;
+        }
     }
 }
