1286193e6944bfa34f2fb61519cbbdba0444f323
lelinh <lelinhmtatn@gmail.com>
2020-10-22 11:26:56 +0700

    CellClick

--- QuanLyKhoHang/GUI/User_Control/Sanpham.cs
+++ QuanLyKhoHang/GUI/User_Control/Sanpham.cs
@@ -14,11 +14,31 @@ namespace QuanlyKhohang.GUI
 {
     public partial class Sanpham : UserControl
     {
-       
+        Sanpham_BUS sp = new Sanpham_BUS();
         public Sanpham()
         {
             InitializeComponent();
-           
+            sp.bangDuLieu = dataGridView1;
+        }
+        private void Sanpham_Load(object sender, EventArgs e)
+        {
+            sp.ViewAll();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            txtSPID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtTenSP.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtNCC.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtGia.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txtSoluong.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            txtNCCID.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+        }
+        public void GetValue(string id, string name)
+        {
+            txtNCCID.Text = id;
+            txtNCC.Text = name;
         }
     }
 }
