e970e86960e3325615f63d36f6f22b8149829159
lelinh <lelinhmtatn@gmail.com>
2020-11-10 16:14:03 +0700

    load_sp

--- QuanLyKhoHang/GUI/User_Control/Sanpham.Designer.cs
+++ QuanLyKhoHang/GUI/User_Control/Sanpham.Designer.cs
@@ -350,6 +350,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Sanpham";
             this.Size = new System.Drawing.Size(869, 541);
+            this.Load += new System.EventHandler(this.Sanpham_Load_1);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
