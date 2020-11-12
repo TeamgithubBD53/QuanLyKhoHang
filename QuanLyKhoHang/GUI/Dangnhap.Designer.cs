da76d65354a81a40a45f1c55bee3b9dfb90b5766
Duongtsnc1 <duongbui.hlnm@gmail.com>
2020-11-12 20:51:21 +0700

    login

--- QuanLyKhoHang/GUI/Dangnhap.Designer.cs
+++ QuanLyKhoHang/GUI/Dangnhap.Designer.cs
@@ -70,6 +70,7 @@
             this.txtMatkhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtMatkhau.Location = new System.Drawing.Point(152, 125);
             this.txtMatkhau.Name = "txtMatkhau";
+            this.txtMatkhau.PasswordChar = '*';
             this.txtMatkhau.Size = new System.Drawing.Size(146, 26);
             this.txtMatkhau.TabIndex = 2;
             // 
--- QuanLyKhoHang/Program.cs
+++ QuanLyKhoHang/Program.cs
@@ -17,7 +17,7 @@ namespace QuanlyKhohang
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new QLKhohang());
+            Application.Run(new Dangnhap());
         }
     }
 }
